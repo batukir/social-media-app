151a9f03ff03992a578e0538b5c5a3e5aa1c375c
BatuhanKir <batuhankir7@gmail.com>
2023-07-13 10:22:47 -0400

    folders

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { BroweserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <BroweserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="home" element={<HomePage/>}/>
+        <Route path="/profile/:userId" element={<ProfilePage/>}/>
+      </Routes>
+      </BroweserRouter>
+    </div>
+  );
+}
+
+export default App;
